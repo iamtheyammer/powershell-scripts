fb4fed6d0d60e0d7cfe799402a697f6ff3f96b68
I Yam Who I Yam <32084206+iamtheyammer@users.noreply.github.com>
2018-10-16 10:53:57 -0700

    Add exporting with Device ID, add AssetTiger ready import

--- SerialsToAssetIds.ps1
+++ SerialsToAssetIds.ps1
@@ -1,10 +1,25 @@
-# Imports a CSV from headers |assetId|serialnumber|. Then, updates admin console asset IDs.
-# Also prints out the number of the row that it's working on so you can see progress.
+# Imports a CSV from headers |assetId|serialNumber|. Then, updates admin console asset IDs.
+# Make sure to change your paths!
 # MIT License by GitHub/iamtheyammer
-$csv = Import-Csv -Path "C:\Users\IT-Master-Lenovo\Downloads\assets.csv"
+
+<# $csv = Import-Csv -Path "C:\Users\IT-Master-Lenovo\Downloads\assets.csv"
 $ids = $null;
 for ($i = 0; $i -lt $csv.Count; $i++) {
     $deviceId = (gam.exe print cros query `"id:$($csv[$i].serialNumber)`" 2> $null).Split("\n")[1] + "`n"
     (gam.exe update cros $($deviceId) assetid $($csv[$i].assetId) 2> $null)
-    $i
+    $i + 1
 }
+#>
+
+# This code is functionally identical except it's made for an AssetTiger import: It uses columns |Serial No|Device ID|Asset Tag ID|
+$csv = Import-Csv -Path "C:\Users\IT-Master-Lenovo\Downloads\assets.csv"
+$ids = $null;
+for ($i = 0; $i -lt $csv.Count; $i++) {
+    $deviceId = (gam.exe print cros query `"id:$($csv[$i]."Serial No")`" 2> $null).Split("\n")[1] # gets the device ID
+    $csv[$i]."Device ID" = $deviceId; # sets the device ID 
+    (gam.exe update cros $($deviceId) assetid $($csv[$i]."Asset Tag ID") 2> $null) # sets the asset tag in google
+    $i + 1
+}
+
+
+$csv | Export-Csv -Path "C:\Users\IT-Master-Lenovo\Downloads\assets.csv" -NoTypeInformation # exports CSV with device 
